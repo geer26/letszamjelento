0d5898524de21fb26ca95aed1a1abcd18cf35b9b
geer26 <gergo.kurucz@gmail.com>
2022-08-31 11:32:57 +0200

    v0.0

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    #return "Hello, World!"
+    user = {'username': 'geer'}
+    return render_template('welcome.html')
